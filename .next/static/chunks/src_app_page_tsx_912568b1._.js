1b2a05560f35f1e12efba837fef6fb0c5b1b0034
Suman Samanta <68841004+sumnsmnt@users.noreply.github.com>
2025-06-05 12:38:39 +0530

    Add .next/static/chunks/src_app_page_tsx_912568b1._.js 1749107319209

---
+++ .next/static/chunks/src_app_page_tsx_912568b1._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_8df213e1._.js"
+],
+    source: "dynamic"
+});
